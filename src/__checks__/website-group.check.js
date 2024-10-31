c2472be86c4c3090eebbce8d794a8ae2e90dcb4b
Maddy Capone <maddycapone@gmail.com>
2024-10-31 09:03:15 -0400

    update website group js

--- src/__checks__/website-group.check.js
+++ src/__checks__/website-group.check.js
@@ -1,6 +1,6 @@
-const { CheckGroup, RetryStrategyBuilder } = require('checkly/constructs');
-const { smsChannel, emailChannel } = require('../alert-channels');
-const alertChannels = [smsChannel, emailChannel];
+const { CheckGroup, RetryStrategyBuilder } = require("checkly/constructs");
+const { emailChannel } = require("../alert-channels");
+const alertChannels = [emailChannel];
 /*
  * In this example, we bundle checks using a Check Group. We add checks to this group in two ways:
  * 1. By passing the `CheckGroup` object for the `group` property of the check.
@@ -13,13 +13,13 @@ const alertChannels = [smsChannel, emailChannel];
  * You can use either or both. In this example we show option 1.
  **/
 
-const websiteGroup = new CheckGroup('website-check-group-1', {
-  name: 'Website Group',
+const websiteGroup = new CheckGroup("website-check-group-1", {
+  name: "Website Group",
   activated: true,
   muted: false,
-  runtimeId: '2024.02',
-  locations: ['us-east-1', 'eu-west-1'],
-  tags: ['mac', 'group'],
+  runtimeId: "2024.02",
+  locations: ["us-east-1", "eu-west-1"],
+  tags: ["mac", "group"],
   environmentVariables: [],
   apiCheckDefaults: {},
   concurrency: 100,
@@ -28,7 +28,11 @@ const websiteGroup = new CheckGroup('website-check-group-1', {
    * Failed check runs in this group will be retried before triggering alerts.
    * The wait time between retries will increase linearly: 30 seconds, 60 seconds, and then 90 seconds between the retries.
    */
-  retryStrategy: RetryStrategyBuilder.linearStrategy({ baseBackoffSeconds: 30, maxRetries: 3, sameRegion: false }),
+  retryStrategy: RetryStrategyBuilder.linearStrategy({
+    baseBackoffSeconds: 30,
+    maxRetries: 3,
+    sameRegion: false,
+  }),
   runParallel: true,
 });
 
